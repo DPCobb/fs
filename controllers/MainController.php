6dee9fabcf76d8abd97d65bf6d5fd943e8641351
Dan Cobb <danielcobb@daniels-MacBook-Pro.local>
2016-11-02 16:38:54 -0400

    Require ++
    Adding required files to index.php and /models/MainController.php

---
+++ controllers/MainController.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * PROJECT: Formstack Assessment
+ * AUTHOR: Daniel Cobb
+ * CREATED: 11/2016
+ */
+
+// Require needed model files
+require './models/Views.Class.php';
+require './models/CreateData.Class.php';
+require './models/ReadData.Class.php';
+require './models/UpdateData.Class.php';
+require './models/DeleteData.Class.php';
+require './models/CreateData.Class.php';
+require './models/DBCred.php';
+
+
+
+
+?>
--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
-    echo 'Hello World';
- ?>
+/*
+ * PROJECT: Formstack Assessment
+ * AUTHOR: Daniel Cobb
+ * CREATED: 11/2016
+ */
+
+// Require the MainController to direct traffic
+require 'controllers/MainController.php';
+
+?>
