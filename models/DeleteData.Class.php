0e48b9ff9fdaad4a9ad97290d7677d2e6fbf234b
Dan Cobb <danielcobb@daniels-MacBook-Pro.local>
2016-11-03 16:51:01 -0400

    DeleteData.Class.php ++
    Added DeleteData.Class model for removing entries

---
+++ models/DeleteData.Class.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ * PROJECT: Formstack Assessment
+ * AUTHOR: Daniel Cobb
+ * CREATED: 11/2016
+ */
+
+class DeleteData
+{
+
+    public $msg;
+
+    /*
+     * deleteData Function
+     *
+     * deleteData removes a specific users information from the database
+     *
+     *
+     * @param $id
+     */
+    function deleteData($id)
+    {
+        include 'DBCred.php';
+        $sth = $dbh->prepare("DELETE FROM user WHERE id = :id");
+        $sth->bindParam(':id', $id);
+        if($sth->execute())
+        {
+            $this->msg = '<h2>User successfully removed.</h2>';
+        }
+        else
+        {
+            $this->msg = '<h2>Could not remove user</h2>';
+        }
+
+        return $this->msg;
+    }
+}
+?>
