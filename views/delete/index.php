33ee8e5336a9223a54c0280303302e8aeb6f2cd1
Dan Cobb <danielcobb@daniels-MacBook-Pro.local>
2016-11-03 17:03:40 -0400

    views/delete/index.php ++
    Display the delete user information

---
+++ views/delete/index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * PROJECT: Formstack Assessment
+ * AUTHOR: Daniel Cobb
+ * CREATED: 11/2016
+ */
+
+require './views/index/DeleteView.Class.php';
+
+$view = new DeleteView('Remove User');
+$view->buildDisplay();
+
+?>
