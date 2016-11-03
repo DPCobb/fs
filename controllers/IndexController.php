986652e6d68e3c1a31a2f6a30800fd2de2e15fc9
Dan Cobb <danielcobb@daniels-MacBook-Pro.local>
2016-11-03 16:43:21 -0400

    Display Changes
    Minor display changes

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -25,17 +25,19 @@ class IndexController
             <th>Last Name</th>
             <th>Email</th>
             <th>Password</th>
+            <th>Remove Entry</th>
         </tr>
         </thead>
         <tbody>';
         foreach($results as $row) {
             echo '
             <tr>
-                <td>ID : ' . $row['id'] . '</td>
-                <td>First Name : ' . $row['firstname'] . '</td>
-                <td>Last Name : ' . $row['lastname'] . '</td>
-                <td>Email : ' . $row['email'] . '</td>
-                <td>Password : ' . $row['password'] . '</td>
+                <td>' . $row['id'] . '</td>
+                <td>' . $row['firstname'] . '</td>
+                <td>' . $row['lastname'] . '</td>
+                <td>' . $row['email'] . '</td>
+                <td>' . $row['password'] . '</td>
+                <td><a class="button" href="index.php?p=delete&id=' . $row['id'] . '">Delete</a></td>
             </tr>
             ';
         }
