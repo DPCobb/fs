a639eefd666ec0337b096369f52bb588c1b7e8a6
Dan Cobb <danielcobb@daniels-MacBook-Pro.local>
2016-11-03 20:11:25 -0400

    ReadView.Class.php
    Added ReadView.Class.php which interacts with the ReadController to
    build the view and get data, either all user data or user specific data

---
+++ views/read/ReadView.Class.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * PROJECT: Formstack Assessment
+ * AUTHOR: Daniel Cobb
+ * CREATED: 11/2016
+ */
+
+require './views/View.Class.php';
+require './controllers/ReadController.php';
+class ReadView extends View
+{
+
+    function __construct($title)
+    {
+        parent::__construct($title);
+    }
+
+    function contentBuild()
+    {
+        $data = new ReadController();
+        echo $data->dashboard();
+    }
+
+    function buildDisplay()
+    {
+        $all = $this->createHeader() . $this->contentBuild() . $this->createFooter();
+        echo $all;
+    }
+}
+
+?>
