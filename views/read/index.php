86cbe30db08ddc4013d01e15992f1438dc445b63
Dan Cobb <danielcobb@daniels-MacBook-Pro.local>
2016-11-03 20:10:35 -0400

    read/index.php ++
    Created the index page for read, calls ReadView.Class to build view

---
+++ views/read/index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * PROJECT: Formstack Assessment
+ * AUTHOR: Daniel Cobb
+ * CREATED: 11/2016
+ */
+
+require './views/read/ReadView.Class.php';
+
+$view = new ReadView('View User Details');
+$view->buildDisplay();
+
+?>
