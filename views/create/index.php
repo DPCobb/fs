aa3ff7ec57cdb0b1f4fe446c3b01b9e194033a7e
Dan Cobb <danielcobb@daniels-MacBook-Pro.local>
2016-11-04 22:17:34 -0400

    create/index.php ++
    Added the main view page for adding a new user

---
+++ views/create/index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * PROJECT: Formstack Assessment
+ * AUTHOR: Daniel Cobb
+ * CREATED: 11/2016
+ */
+
+require './views/create/CreateView.Class.php';
+
+$view = new CreateView('Add a new User');
+$view->buildDisplay();
+
+?>
