8f80e543af4b394dfb5f23f49242bdc579c94068
Dan Cobb <danielcobb@daniels-MacBook-Pro.local>
2016-11-05 21:46:15 -0400

    update/index.php ++
    Added index.php for the update view

---
+++ views/update/index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * PROJECT: Formstack Assessment
+ * AUTHOR: Daniel Cobb
+ * CREATED: 11/2016
+ */
+
+require './views/update/UpdateView.Class.php';
+
+$view = new UpdateView('Update A User');
+$view->buildDisplay();
+
+?>
