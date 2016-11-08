a25bbdeaed1bebdea19ce458647c766e0e207412
Dan Cobb <danielcobb@daniels-MacBook-Pro.local>
2016-11-08 18:52:50 -0500

    DeleteController Refactor
    Removing some unneeded code and assigning id in __construct

--- controllers/DeleteController.php
+++ controllers/DeleteController.php
@@ -9,11 +9,21 @@ namespace delete_control;
 use \delete_data as delete_data;
 
 require './models/DeleteData.Class.php';
+
 class DeleteController
 {
     private $userId;
+
+    /**
+     * Assigns value to userId.
+     */
     public function __construct()
     {
+        // if url param id is not empty
+        if (!empty($_GET['id'])) {
+            // userid is equal to $_GET id
+            $this->userId = $_GET['id'];
+        }
     }
 
     /**
@@ -22,14 +32,9 @@ class DeleteController
      */
     public function deleteUser()
     {
-        // if url param id is not empty
-        if (!empty($_GET['id'])) {
-            // userid is equal to $_GET id
-            $this->userId = $_GET['id'];
-            // instantiate DeleteData
-            $data = new delete_data\DeleteData($this->userId);
-            // echo the results of deleteData
-            return $data->deleteData($this->userId) . '<a href="index.php" class="button">Home</a>';
-        }
+        // instantiate DeleteData
+        $data = new delete_data\DeleteData($this->userId);
+        // echo the results of deleteData
+        return $data->deleteData($this->userId) . '<a href="index.php" class="button">Home</a>';
     }
 }
