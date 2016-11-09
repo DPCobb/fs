c705c3f1f4f6c204f70729789643a59376b22040
Dan Cobb <danielcobb@daniels-MacBook-Pro.local>
2016-11-08 20:11:41 -0500

    IndexController: Refactor
    Rewrote IndexController to use HtmlController for HTML generation

---
+++ controllers/IndexController.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * PROJECT: Formstack Assessment
+ * AUTHOR: Daniel Cobb
+ * CREATED: 11/2016
+ */
+namespace index_control;
+
+use \read_data as read_data;
+use \html_control as html_control;
+
+require 'HtmlController.php';
+require './models/ReadData.Class.php';
+class IndexController
+{
+    private $html;
+
+    public function __construct()
+    {
+        $this->html = new html_control\HtmlController;
+    }
+
+    /**
+     * dashboard echos a parsed data set for the main index.php page.
+     * @return null
+     */
+    public function mainView()
+    {
+        // Fetch all the users from the DB
+        $data = new read_data\ReadData();
+        $results = $data->getAllData();
+        // build out the results in a table
+        $this->html->mainViewHTML($results);
+
+    }
+}
