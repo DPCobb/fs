7e7d4a377e0d916d9c39d767d3614789fe39945b
Dan Cobb <danielcobb@daniels-MacBook-Pro.local>
2016-11-08 20:27:41 -0500

    HtmlController ++
    Added HTML for update and create

--- controllers/HtmlController.php
+++ controllers/HtmlController.php
@@ -9,12 +9,11 @@ namespace html_control;
 class HtmlController
 {
     /**
-     * dashboard echos a parsed data set for the main index.php page.
+     * mainViewHTML echoes a parsed data set for the main index.php page.
      * @return null
      */
     public function mainViewHTML($results)
     {
-        // echo out the results in a table
         echo '
         <table>
         <thead>
@@ -48,6 +47,11 @@ class HtmlController
         echo '</tbody></table><a class="button" href="index.php?p=create">Add User</a>';
     }
 
+    /**
+     * readViewHTML builds out the table for ReadController.
+     * @param  mixed $results array of results from db
+     * @return null
+     */
     public function readViewHTML($results)
     {
         echo'
@@ -79,5 +83,47 @@ class HtmlController
             ';
         }
         echo '</tbody></table>';
+    }
+
+    /**
+     * updateViewHtml creates the form for updating user info.
+     * @param  mixed $results array of user details
+     * @param  integer $id      user id
+     * @return null
+     */
+    public function updateViewHtml($results, $id)
+    {
+        foreach ($results as $row) {
+            echo '
+            <form method="POST" action="index.php?p=update&id=' . $id . '&action=updateuser">
+                <label for="email">Email</label>
+                <input type="text" value=' . $row['email'] . ' name="email" id="email"/>
+                <label for="first">Firstname</label>
+                <input type="text" value=' . $row['firstname'] . ' name="first" id="first"/>
+                <label for="last">Lastname</label>
+                <input type="text" value=' . $row['lastname'] . ' name="last" id="last"/>
+                <label for="pass">Password</label>
+                <input type="text" value=' . $row['password'] . ' name="pass" id="pass"/>
+                <input type="submit" value="Update User"/>
+            </form>
+            ';
+        }
+    }
+
+    /**
+     * createViewHtml creates the new user form for CreateController.
+     * @return string returns the form
+     */
+    public function createViewHtml()
+    {
+        return '
+        <form method="post" action="index.php?p=create&action=add" id="newuser">
+            <input type="text" name="first" id="firstname" placeholder="Enter your first name" required/>
+            <input type="text" name="last" id="lastname" placeholder="Enter your last name" required/>
+            <input type="email" name="email" id="email" placeholder="Enter your email" required/>
+            <input type="text" name="password" id="pass" placeholder="Enter your password" required/>
+            <input type="submit" value="Create"/>
+        </form>
+        ';
     }
 }
