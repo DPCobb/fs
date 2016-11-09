01894024a784fdca8c986f1c32e3e560a3d3f3da
Dan Cobb <danielcobb@daniels-MacBook-Pro.local>
2016-11-08 20:10:32 -0500

    HtmlController ++
    Added an HTML controller to consolidate HTML output into one class for
    easier editing.

--- controllers/HtmlController.php
+++ controllers/HtmlController.php
@@ -4,26 +4,16 @@
  * AUTHOR: Daniel Cobb
  * CREATED: 11/2016
  */
-namespace index_control;
+namespace html_control;
 
-use \read_data as read_data;
-
-require './models/ReadData.Class.php';
-class IndexController
+class HtmlController
 {
-    public function __construct()
-    {
-    }
-
     /**
      * dashboard echos a parsed data set for the main index.php page.
      * @return null
      */
-    public function dashboard()
+    public function mainViewHTML($results)
     {
-        // Fetch all the users from the DB
-        $data = new read_data\ReadData();
-        $results = $data->getAllData();
         // echo out the results in a table
         echo '
         <table>
@@ -56,5 +46,38 @@ class IndexController
             ';
         }
         echo '</tbody></table><a class="button" href="index.php?p=create">Add User</a>';
+    }
+
+    public function readViewHTML($results)
+    {
+        echo'
+        <table>
+        <thead>
+        <tr>
+            <th>ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Password</th>
+            <th>Update Entry</th>
+            <th>Remove Entry</th>
+        </tr>
+        </thead>
+        <tbody>';
+        // use foreach loop to parse data set
+        foreach ($results as $row) {
+            echo '
+            <tr>
+                <td>' . $row['id'] . '</td>
+                <td>' . $row['firstname'] . '</td>
+                <td>' . $row['lastname'] . '</td>
+                <td>' . $row['email'] . '</td>
+                <td>' . $row['password'] . '</td>
+                <td><a class="button" href="index.php?p=update&id=' . $row['id'] . '">Update</a></td>
+                <td><a class="button" href="index.php?p=delete&id=' . $row['id'] . '">Delete</a></td>
+            </tr>
+            ';
+        }
+        echo '</tbody></table>';
     }
 }
